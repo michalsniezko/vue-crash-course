3e85070217c06768c2b99d2d4d00216091edf9ae
Michał Śnieżko <michal.sniezko@softwaremind.com>
2019-10-30 11:27:34 +0100

    Implement todoItem and markComplete method

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
+import Todos from "./components/Todos";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: true
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
 </style>
